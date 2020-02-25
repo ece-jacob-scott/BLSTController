f483f47401967c18dd4ca253f3efa49f3c044574
Jacob Scott <jscott35@uic.edu>
2020-02-24 19:10:23 -0600

    Updated github link in setup file

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="BLSTController", # Replace with your own username
+    name="BLSTController",
     version="0.0.1",
     author="Jacob Scott",
     author_email="jscott35@uic.edu",
     description="Main library for allowing control of BLST foot pedals.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/ece-jacob-scott/BLSTController",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
