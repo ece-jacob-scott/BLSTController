b0df178a31bdf7fa03b420ec8827a5a7c707e2b0
Jacob Scott <jscott35@uic.edu>
2020-02-26 16:56:54 -0600

    Adds class imports to the __init__.py file
    Now all classes (besides utils) can be imported using just the
    BLSTController module.

---
+++ BLSTController/__init__.py
@@ -0,0 +1,6 @@
+"""
+Importing these objects in _init_.py makes them available on the BLSTController
+module. 
+"""
+from BLSTController.Action import Action
+from BLSTController.Controller import Controller, Observer
